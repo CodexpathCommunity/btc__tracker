3f924786303f584a836f9a3165872c1a5530afa8
Skiboybl <69096827+CodexpathCommunity@users.noreply.github.com>
2021-04-09 08:40:18 +0100

    added the markup with data from the api

--- components/Coins/index.js
+++ components/Coins/index.js
@@ -1,8 +1,35 @@
-const Coins = ({ name, image }) => {
+const Coins = ({
+  name,
+  image,
+  key,
+  id,
+  price,
+  symbol,
+  marketcap,
+  volume,
+  priceChange,
+}) => {
   return (
-    <div>
-      <h2> {name} </h2>
-      <img src={image} alt="" />
+    <div className="coin_container">
+      <div className="coin_row">
+        <div className="coin">
+          <img src={image} alt={name} className="coin_img" />
+          <h1 className="coin_h1">{name}</h1>
+          <p className="coin_symbol"> {symbol} </p>
+        </div>
+        <div className="coin_data">
+          <p className="coin_price">$ {price} </p>
+          <p className="coin_volume">$ {volume.toLocaleString()} </p>
+          {priceChange < 0 ? (
+            <p className="coin_percent red">{priceChange.toFixed(2)} %</p>
+          ) : (
+            <p className="coin_percent green">{priceChange.toFiced(2)}%</p>
+          )}
+          <p className="coin_marketcap">
+            Mkt Cap: ${marketcap.toLocaleString()}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
